e5a17b3463ec718ecf711d852d12f906f59d65c5
Sjerega <Sjerega@yandex.ru>
2021-07-21 15:37:11 +0300

    add scene 1 sky

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  let scene1Style = document.getElementById('scene-1').style
   let hallStyle = document.getElementById('scene-1-hall').style;
   let contentStyle = document.getElementById('scene-1-content').style;
   let wallStyle = document.getElementById('scene-1-wall').style;
@@ -9,6 +10,7 @@ window.onload = () => {
   let rendering = () => {
     position = docElem.scrollTop / viewportHeight;
 
+    scene1Style.backgroundPositionY = `${position<.7 ? position*100 : 70}%`
     hallStyle.transform = `scale(${ position<.7 ? position/.7+1 : 2 }) translateY(${ position<.7 ? position*35 : 24.5 }%)`;
     contentStyle.opacity = position<.69 ? 0 : 1;
     wallStyle.transform = `translateY(${ position<.5 ? position*125 : 62.5 }%)`;

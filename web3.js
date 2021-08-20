af96c36b2da82f700a4a80d5c90b2a64e2c68f99
TorfinnrKarlsefni <keonenuri@gmail.com>
2021-08-21 06:22:43 +0700

    pre-sale vse

--- web3.js
+++ web3.js
@@ -4,7 +4,7 @@ const EvmChains = window.evmChains;
 const Fortmatic = window.Fortmatic;
 const BN = window.Web3.utils.BN;
 
-let countDownDate = new Date(Date.UTC(2021, 7, 20, 22));
+let countDownDate = new Date(Date.UTC(2021, 7, 23, 22));
 
 const contractAddress = "0x020BB206cd689d6981182579da490d5F4ceB4c46";
 const contractABI = [
